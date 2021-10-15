057d9f3c7eff2c5498710b3cdc7377b63eb7fac2
Rishibh <r.prakash@mail.utoronto.ca>
2021-10-15 11:54:28 -0400

    Added 'Axes' object for storing Expressions

---
+++ src/main/java/Axes.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Axes {
+
+    List<Expression> expressionList;
+
+    public void addExpression(Expression expr){
+        expressionList.add(expr);
+    }
+
+}
