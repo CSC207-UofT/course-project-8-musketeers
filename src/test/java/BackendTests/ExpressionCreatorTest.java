0e0fd7a2089eefd4c20b9c70681a293275e74528
dniloy <dihan8917@gmail.com>
2021-10-22 15:45:43 -0400

    Changed Arrays.asList to List.of in ExpressionCreatorTest to avoid some errors.

--- src/test/java/BackendTests/ExpressionCreatorTest.java
+++ src/test/java/BackendTests/ExpressionCreatorTest.java
@@ -39,27 +39,27 @@ public class ExpressionCreatorTest {
 
     @Test(timeout = 50)
     public void testBinaryOperatorTwoTerms(){
-        Expression exp = ec.create(Arrays.asList("2", "+", "3"));
+        Expression exp = ec.create(List.of("2", "+", "3"));
         assertEquals(exp.evaluate(varMap), 5.0, delta);
     }
 
     // Bracket parsing to be implemented
     @Test(timeout = 50)
     public void testBinaryOperatorTwoTermsWithBrackets(){
-        Expression exp = ec.create(Arrays.asList("2", "*", "(", "3", ")"));
+        Expression exp = ec.create(List.of("2", "*", "(", "3", ")"));
         assertEquals(exp.evaluate(varMap), 6.0, delta);
     }
 
     @Test(timeout = 50)
     public void testBinaryOperatorWithOneVariable(){
-        Expression exp = ec.create(Arrays.asList("x", "/", "3"));
+        Expression exp = ec.create(List.of("x", "/", "3"));
         varMap.put("x", 3.0);
         assertEquals(exp.evaluate(varMap), 1.0, delta);
     }
 
     @Test(timeout = 50)
     public void testBinaryOperatorWithTwoVariables(){
-        Expression exp = ec.create(Arrays.asList("x", "/", "y"));
+        Expression exp = ec.create(List.of("x", "/", "y"));
         varMap.put("x", 3.0);
         varMap.put("y", 6.0);
         assertEquals(exp.evaluate(varMap), 0.5, delta);
@@ -67,32 +67,32 @@ public class ExpressionCreatorTest {
 
     @Test(timeout = 50)
     public void testOrderOfOperations1(){
-        Expression exp = ec.create(Arrays.asList("1.5", "+", "3", "*", "5"));
+        Expression exp = ec.create(List.of("1.5", "+", "3", "*", "5"));
         assertEquals(exp.evaluate(varMap), 16.5, delta);
     }
 
     @Test(timeout = 50)
     public void testOrderOfOperations2(){
-        Expression exp = ec.create(Arrays.asList("4", "*", "3", "-", "2"));
+        Expression exp = ec.create(List.of("4", "*", "3", "-", "2"));
         assertEquals(exp.evaluate(varMap), 10.0, delta);
     }
 
     @Test(timeout = 50)
     public void testOrderOfOperations3(){
-        Expression exp = ec.create(Arrays.asList("x", "*", "3", "-", "2"));
+        Expression exp = ec.create(List.of("x", "*", "3", "-", "2"));
         varMap.put("x", 4.0);
         assertEquals(exp.evaluate(varMap), 10.0, delta);
     }
 
     @Test(timeout = 50)
     public void testRepeatedOperations(){
-        Expression exp = ec.create(Arrays.asList("2", "-", "1", "-", "3"));
+        Expression exp = ec.create(List.of("2", "-", "1", "-", "3"));
         assertEquals(exp.evaluate(varMap), -2, delta);
     }
 
     @Test(timeout = 50)
     public void testRepeatedOperations2(){
-        Expression exp = ec.create(Arrays.asList("2", "/", "4", "/", "10"));
+        Expression exp = ec.create(List.of("2", "/", "4", "/", "10"));
         assertEquals(exp.evaluate(varMap), 0.05, delta);
     }
 
@@ -100,49 +100,49 @@ public class ExpressionCreatorTest {
 
     @Test(timeout = 50)
     public void testPower(){
-        Expression exp = ec.create(Arrays.asList("x", "^", "2"));
+        Expression exp = ec.create(List.of("x", "^", "2"));
         varMap.put("x", 4.0);
         assertEquals(exp.evaluate(varMap), 16.0, delta);
     }
 
     @Test(timeout = 50)
     public void testCos(){
-        Expression exp = ec.create(Arrays.asList("cos", "(", "x", "^", "2", ")"));
+        Expression exp = ec.create(List.of("cos", "(", "x", "^", "2", ")"));
         varMap.put("x", 1.0);
         assertEquals(exp.evaluate(varMap), Math.cos(1), delta);
     }
 
     @Test(timeout = 50)
     public void testCosWithOtherOperators(){
-        Expression exp = ec.create(Arrays.asList("2", "*", "cos", "(", "x", "^", "2", ")", "+", "5"));
+        Expression exp = ec.create(List.of("2", "*", "cos", "(", "x", "^", "2", ")", "+", "5"));
         varMap.put("x", 0.0);
         assertEquals(exp.evaluate(varMap), 7, delta);
     }
 
     @Test(timeout = 50)
     public void testSin(){
-        Expression exp = ec.create(Arrays.asList("sin", "(", "x", "^", "2", ")"));
+        Expression exp = ec.create(List.of("sin", "(", "x", "^", "2", ")"));
         varMap.put("x", 1.0);
         assertEquals(exp.evaluate(varMap), Math.sin(1), delta);
     }
 
     @Test(timeout = 50)
     public void testSinWithOtherOperators(){
-        Expression exp = ec.create(Arrays.asList("cos", "(", "x", ")", "^", "2", "+", "sin", "(", "x", ")", "^", "2"));
+        Expression exp = ec.create(List.of("cos", "(", "x", ")", "^", "2", "+", "sin", "(", "x", ")", "^", "2"));
         varMap.put("x", 3.0);
         assertEquals(exp.evaluate(varMap), 1, delta);
     }
 
     @Test(timeout = 50)
     public void testTan(){
-        Expression exp = ec.create(Arrays.asList("tan", "(", "x", "^", "2", ")"));
+        Expression exp = ec.create(List.of("tan", "(", "x", "^", "2", ")"));
         varMap.put("x", 1.0);
         assertEquals(exp.evaluate(varMap), Math.tan(1), delta);
     }
 
     @Test(timeout = 50)
     public void testTanWithOtherOperators(){
-        Expression exp = ec.create(Arrays.asList("tan", "(", "x", ")", "^", "2", "+", "3"));
+        Expression exp = ec.create(List.of("tan", "(", "x", ")", "^", "2", "+", "3"));
         varMap.put("x", 1.0);
         assertEquals(exp.evaluate(varMap), Math.pow(Math.tan(1), 2.0) + 3, delta);
     }
