124f550d4ef24051e6e0f375a15512e241da913f
dniloy <dihan8917@gmail.com>
2021-10-24 14:57:01 -0400

    Added logical operators.

--- src/main/java/Backend/Constants.java
+++ src/main/java/Backend/Constants.java
@@ -6,7 +6,8 @@ import java.util.Set;
 
 public class Constants {
 
-    private final List<String> OPERATORS = new ArrayList<>(List.of(new String[]{"-", "+", "*", "/", "^"}));
+    private final List<String> OPERATORS = new ArrayList<>(List.of(new String[]{"&", "|", "!", "-",
+            "+", "*", "/", "^"}));
     private final List<String> COMPARISONS = new ArrayList<>(List.of(new String[]{"<=", ">=", "<", ">"}));
     private final Set<String> VARIABLES = Set.of("x", "y", "z");
     private final Set<String> FUNCTIONS = Set.of("cos", "sin", "tan", "sqrt");
