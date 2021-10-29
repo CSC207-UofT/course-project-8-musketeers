f4b893263bfe056689af058c11e74cfd9768524b
Andrew-Huangg <at.huang@mail.utoronto.ca>
2021-10-29 01:41:13 -0400

    Origin before could only take integer co-ordinates, quickly changed it so that it can take floats

--- src/main/java/Backend/Axes.java
+++ src/main/java/Backend/Axes.java
@@ -1,10 +1,9 @@
 package Backend;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 //        - stores a 'scale' attribute (type float)
 //        - stores attributes for the x and y coordinates of the origin
@@ -13,12 +12,18 @@ import java.util.Set;
 //        - a getter for the collection of Expressions
 public class Axes {
     private float scale;
-    private Point origin;
+    private Point2D.Float origin;
     private final List<Expression> exprCollection; //should be final right??
 
     public Axes(){
         this.scale = 1;
-        this.origin = new Point();
+        this.origin = new Point2D.Float();
+        this.exprCollection = new ArrayList<>();
+
+    }
+    public Axes(float a, float b, float c){
+        this.scale = a;
+        this.origin = new Point2D.Float(b, c);
         this.exprCollection = new ArrayList<>();
 
     }
@@ -31,11 +36,11 @@ public class Axes {
 //    public int getxO(){return this.xO;}
 //    public int gety0(){return this.y0;}
 
-    public Point getOrigin(){return this.origin;}
+    public Point2D.Float getOrigin(){return this.origin;}
 
-    public void setOrigin(int x, int y){this.origin = new Point(x,y);}
+    public void setOrigin(float x, float y){this.origin = new Point2D.Float(x, y);}
     //overload setter origin. can take a point or 2 ints
-    public void setOrigin(Point p){this.origin = p;}
+    public void setOrigin(Point2D.Float p){this.origin = p;}
 
 
 
--- src/test/java/BackendTests/AxesTest.java
+++ src/test/java/BackendTests/AxesTest.java
@@ -7,8 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.*;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -34,19 +34,29 @@ public class AxesTest {
     public void testAxesCreation(){
         assertEquals(ax.getScale(),1,0);
         assertEquals(ax.getOrigin(),
-                new Point(0,0));
+                new Point2D.Float(0,0));
         assertEquals(ax.getExpressions(), new ArrayList<Expression>() {
         });
     }
 
     @Test(timeout = 50)
+    public void testAxesCreation2(){
+        ax = new Axes(5, 6, 7);
+
+        assertEquals(ax.getScale(),5,0);
+        assertEquals(ax.getOrigin(),
+                new Point2D.Float(6,7));
+        assertEquals(ax.getExpressions(), new ArrayList<Expression>() {});
+    }
+
+    @Test(timeout = 50)
     public void testAxesSetOrigin(){
-        assertEquals(ax.getOrigin(), new Point(0,0));
+        assertEquals(ax.getOrigin(), new Point2D.Float(0,0));
         ax.setOrigin(2,3);
-        assertEquals(ax.getOrigin(), new Point(2,3));
-        Point p = new Point(-5,7);
+        assertEquals(ax.getOrigin(), new Point2D.Float(2,3));
+        Point2D.Float p = new Point2D.Float(-5,7.55f);
         ax.setOrigin(p);
-        assertEquals(ax.getOrigin(),p);
+        assertEquals(ax.getOrigin(),new Point2D.Float(-5,7.55f));
     }
 
     @Test(timeout = 50)
