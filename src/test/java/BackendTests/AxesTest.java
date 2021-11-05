758dd2b7ce8fee7eb7cad0a4b0c2fe32e97419bf
Andrew-Huangg <at.huang@mail.utoronto.ca>
2021-11-05 11:58:32 -0400

    updated AxesTest to work with array origin

--- src/test/java/BackendTests/AxesTest.java
+++ src/test/java/BackendTests/AxesTest.java
@@ -33,8 +33,8 @@ public class AxesTest {
     @Test(timeout = 50)
     public void testAxesCreation(){
         assertEquals(ax.getScale(),1,0);
-        assertEquals(ax.getOrigin(),
-                new Point2D.Float(0,0));
+        float[] p = {0,0};
+        assertArrayEquals(ax.getOrigin(), p, 0.0F);
         assertEquals(ax.getExpressions(), new ArrayList<Expression>() {
         });
     }
@@ -44,26 +44,26 @@ public class AxesTest {
         ax = new Axes(5, 6, 7);
 
         assertEquals(ax.getScale(),5,0);
-        assertEquals(ax.getOrigin(),
-                new Point2D.Float(6,7));
+        assertArrayEquals(ax.getOrigin(), new float[]{6, 7}, 0.0F);
         assertEquals(ax.getExpressions(), new ArrayList<Expression>() {});
     }
 
     @Test(timeout = 50)
     public void testAxesSetOrigin(){
-        assertEquals(ax.getOrigin(), new Point2D.Float(0,0));
+        assertArrayEquals(ax.getOrigin(), new float[]{0, 0}, 0.0F);
+
         ax.setOrigin(2,3);
-        assertEquals(ax.getOrigin(), new Point2D.Float(2,3));
-        Point2D.Float p = new Point2D.Float(-5,7.55f);
-        ax.setOrigin(p);
-        assertEquals(ax.getOrigin(),new Point2D.Float(-5,7.55f));
+        assertArrayEquals(ax.getOrigin(), new float[]{2, 3}, 0.0F);
+
+        ax.setOrigin(new float[]{-5f,7.55f});
+        assertArrayEquals(ax.getOrigin(), new float[]{-5, 7.55f}, 0.0F);
     }
 
     @Test(timeout = 50)
     public void testAxesSetScale(){
-        assertEquals(ax.getScale(), 1F, 0.0001);
-        ax.setScale(3F);
-        assertEquals(ax.getScale(), 3F, 0.0001);
+        assertEquals(ax.getScale(), 1f, 0.0001);
+        ax.setScale(3f);
+        assertEquals(ax.getScale(), 3f, 0.0001);
 
     }
 
