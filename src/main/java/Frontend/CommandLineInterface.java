c8a761cd51a6b1c81134ed885388f3c5a468b895
Andric0901 <binseong.kim@mail.utoronto.ca>
2021-11-12 10:51:18 -0500

    Fixed static issue with acceptedCommands

--- src/main/java/Frontend/CommandLineInterface.java
+++ src/main/java/Frontend/CommandLineInterface.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CommandLineInterface {
-    private static final String[] acceptedCommands = {"-eq", "-save", "-domain", "-2d", "-3d",
-            "-load", "-graph"};
-
     public static void main(String[] args) {
+        String[] acceptedCommands = {"-eq", "-save", "-domain", "-2d", "-3d",
+                "-load", "-graph"};
         ArrayList<String> userInputs = new ArrayList<>(Arrays.asList(args));
         if (userInputs.size() % 2 != 0) {
             // There is an issue -- the (command, response) pair doesn't match up
