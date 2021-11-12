a4467da6596c89a88b110ba84feb63e6a054053f
Andric0901 <binseong.kim@mail.utoronto.ca>
2021-11-11 22:13:03 -0500

    Initial CLI implementation with error catching (NOT COMPLETE), to be implemented as soon as possible

--- src/main/java/Frontend/CommandLineInterface.java
+++ src/main/java/Frontend/CommandLineInterface.java
@@ -1,7 +1,38 @@
 package Frontend;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommandLineInterface {
+    private static final String[] acceptedCommands = {"-eq", "-save", "-domain", "-2d", "-3d",
+            "-load", "-graph"};
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<String> userInputs = new ArrayList<>(Arrays.asList(args));
+        if (userInputs.size() % 2 != 0) {
+            // There is an issue -- the (command, response) pair doesn't match up
+            // Add null at the end for now, and find the issue
+            userInputs.add(null);
+            for (int i = 0; i < userInputs.size(); i += 2) {
+                String firstElementOfPair = userInputs.get(i).toLowerCase();
+                String secondElementOfPair = userInputs.get(i + 1);
+                if (!Arrays.asList(acceptedCommands).contains(firstElementOfPair)) {
+                    System.out.println(firstElementOfPair + " is not a valid command. Please try again.");
+                    break;
+                } else if (secondElementOfPair == null) {
+                    System.out.println(firstElementOfPair + " has no valid response. Please try again.");
+                    break;
+                }
+            }
+        } else {
+            for (int i = 0; i < userInputs.size(); i += 2) {
+                String firstElementOfPair = userInputs.get(i).toLowerCase();
+                String secondElementOfPair = userInputs.get(i + 1);
+                if (!Arrays.asList(acceptedCommands).contains(firstElementOfPair)) {
+                    System.out.println(firstElementOfPair + " is not a valid command. Please try again.");
+                    break;
+                }
+            }
+        }
     }
 }
