2850b637873afc2eae632b7faff3a35fbe9ed353
Andric0901 <binseong.kim@mail.utoronto.ca>
2021-11-12 10:42:12 -0500

    Fixed initial CLI code smell issues, according to the comments

--- src/main/java/Frontend/CommandLineInterface.java
+++ src/main/java/Frontend/CommandLineInterface.java
@@ -13,25 +13,16 @@ public class CommandLineInterface {
             // There is an issue -- the (command, response) pair doesn't match up
             // Add null at the end for now, and find the issue
             userInputs.add(null);
-            for (int i = 0; i < userInputs.size(); i += 2) {
-                String firstElementOfPair = userInputs.get(i).toLowerCase();
-                String secondElementOfPair = userInputs.get(i + 1);
-                if (!Arrays.asList(acceptedCommands).contains(firstElementOfPair)) {
-                    System.out.println(firstElementOfPair + " is not a valid command. Please try again.");
-                    break;
-                } else if (secondElementOfPair == null) {
-                    System.out.println(firstElementOfPair + " has no valid response. Please try again.");
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < userInputs.size(); i += 2) {
-                String firstElementOfPair = userInputs.get(i).toLowerCase();
-                String secondElementOfPair = userInputs.get(i + 1);
-                if (!Arrays.asList(acceptedCommands).contains(firstElementOfPair)) {
-                    System.out.println(firstElementOfPair + " is not a valid command. Please try again.");
-                    break;
-                }
+        }
+        for (int i = 0; i < userInputs.size(); i += 2) {
+            String firstElementOfPair = userInputs.get(i).toLowerCase();
+            String secondElementOfPair = userInputs.get(i + 1);
+            if (!Arrays.asList(acceptedCommands).contains(firstElementOfPair)) {
+                System.out.println(firstElementOfPair + " is not a valid command. Please try again.");
+                break;
+            } else if (secondElementOfPair == null) {
+                System.out.println(firstElementOfPair + " has no valid response. Please try again.");
+                break;
             }
         }
     }
