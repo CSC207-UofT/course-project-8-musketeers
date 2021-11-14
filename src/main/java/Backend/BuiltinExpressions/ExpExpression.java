a6b0cdb6a81ce70faaa125e7d861a31f74535e00
Kevin Wang <kevinj.wang@mail.utoronto.ca>
2021-11-14 09:23:40 -0500

    new function guys! e^stuff

---
+++ src/main/java/Backend/BuiltinExpressions/ExpExpression.java
@@ -0,0 +1,19 @@
+package Backend.BuiltinExpressions;
+
+import Backend.Expression;
+import Backend.FunctionExpression;
+
+import java.util.Map;
+
+public class ExpExpression extends FunctionExpression {
+    // Only one input for exp of course
+    // But the input can be an expression of any kind
+    public ExpExpression(Expression[] inputs){
+        super("exp", inputs);
+    }
+
+    @Override
+    public float evaluate(Map<String, Float> arguments) {
+        return (float) Math.exp(getInputs()[0].evaluate(arguments));
+    }
+}
