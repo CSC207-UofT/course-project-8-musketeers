7641eafc4dac959fa6c7441aa97ff4e71c7d9875
Rishibh <r.prakash@mail.utoronto.ca>
2021-11-14 23:15:36 -0500

    ExpressionReader bug fixed

--- src/main/java/Backend/ExpressionReader.java
+++ src/main/java/Backend/ExpressionReader.java
@@ -34,7 +34,7 @@ public class ExpressionReader {
         }
         else
         {   if (!terms.contains("=")){
-                throw new InvalidTermException("Input interpreted as an explicit function but no '=' found");
+                return realValuedRead(terms);
             }
             if (isExplicit(terms)){
                 return explicitRead(terms);
