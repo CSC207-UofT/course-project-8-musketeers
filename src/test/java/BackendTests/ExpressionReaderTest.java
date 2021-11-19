7f15d19780cd98ad1af40a80304eb0412e1fc81a
dniloy <dihan8917@gmail.com>
2021-11-18 19:07:32 -0500

    Code cleanup: swapped expected and actual arguments in assertEquals calls in test suites to correct places.

--- src/test/java/BackendTests/ExpressionReaderTest.java
+++ src/test/java/BackendTests/ExpressionReaderTest.java
@@ -28,78 +28,78 @@ public class ExpressionReaderTest {
     @Test(timeout = 50)
     public void testBinaryOperatorTwoTerms() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("2 + 3");
-        assertEquals(exp.evaluate(varMap), 5.0, delta);
+        assertEquals(5.0, exp.evaluate(varMap), delta);
     }
 
     // Bracket parsing to be implemented
     @Test(timeout = 50)
     public void testBinaryOperatorTwoTermsWithBrackets() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("2 * (3)");
-        assertEquals(exp.evaluate(varMap), 6.0, delta);
+        assertEquals(6.0, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testBinaryOperatorWithOneVariable() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("x /    3");
         varMap.put("x", 3.f);
-        assertEquals(exp.evaluate(varMap), 1.0, delta);
+        assertEquals(1.0, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testOrderOfOperations1() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("1.5 + 3 * 5");
-        assertEquals(exp.evaluate(varMap), 16.5, delta);
+        assertEquals(16.5, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testOrderOfOperations2() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("4 *  3 - 2");
-        assertEquals(exp.evaluate(varMap), 10.0, delta);
+        assertEquals(10.0, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testRepeatedOperations() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("2 - 1 - 3");
-        assertEquals(exp.evaluate(varMap), -2, delta);
+        assertEquals(-2, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testRepeatedOperations2() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("2 / 2 / 2");
-        assertEquals(exp.evaluate(varMap), 0.5, delta);
+        assertEquals(0.5, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testCos() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("cos(x^2)");
         varMap.put("x", 1.0f);
-        assertEquals(exp.evaluate(varMap), Math.cos(1), delta);
+        assertEquals(Math.cos(1), exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testCosWithOtherOperators() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("2 * cos ( x^2 ) + 5");
         varMap.put("x", 0.0f);
-        assertEquals(exp.evaluate(varMap), 7, delta);
+        assertEquals(7, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testSin() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("sin ( x ^ 2)");
         varMap.put("x", 1.0f);
-        assertEquals(exp.evaluate(varMap), Math.sin(1), delta);
+        assertEquals(Math.sin(1), exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testSinWithOtherOperators() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("cos (x ) ^ 2+sin ( x )^ 2");
         varMap.put("x", 3.0f);
-        assertEquals(exp.evaluate(varMap), 1, delta);
+        assertEquals(1, exp.evaluate(varMap), delta);
     }
 
     @Test(timeout = 50)
     public void testWeirdCombinationsOfOperators() throws InvalidTermException {
         RealValuedExpression exp = (RealValuedExpression) er.read("+-++--+1");
-        assertEquals(exp.evaluate(varMap), -1f, delta);
+        assertEquals(-1f, exp.evaluate(varMap), delta);
     }
 }
