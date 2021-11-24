c4ed107fbd31cafca5bbded42a74a49cd5d5a7c4
Kevin Wang <kevinj.wang@mail.utoronto.ca>
2021-11-23 21:49:56 -0500

    separated writing image to new imagewriter object which has 1 method which is write the image

--- src/main/java/Graphics/Grapher.java
+++ src/main/java/Graphics/Grapher.java
@@ -53,22 +53,8 @@ public class Grapher {
         }
         pixels = ad.drawAxes(pixels, size, size, graphData);
         pixels = ad.drawGrid(pixels, size, size, graphData);
-        writeImage(pixels, size, size, imgName);
-    }
-
-    /** Saves a pixel array as an image
-     * @param pixels Array of integers corresponding to the colour of each pixel
-     * @param iw Width of image (in pixels)
-     * @param ih Height of the image (in pixel)
-     * @param fname Name of file to which we save the image
-     * @throws IOException If there is an error with saving image
-     */
-    private void writeImage(int[] pixels, int iw, int ih, String fname) throws IOException {
-        BufferedImage outImage = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_ARGB);
-
-        outImage.setRGB(0, 0, iw, ih, pixels, 0, iw);
-
-        ImageIO.write(outImage, "png", new File(fname));
+        ImageWriter writer = new ImageWriter();
+        writer.writeImage(pixels, size, size, imgName);
     }
 
     /**
