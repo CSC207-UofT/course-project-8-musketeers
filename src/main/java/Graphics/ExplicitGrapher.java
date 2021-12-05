a9a9168baf702b3f69f6a4e974dd5e54be61c38c
Kevin Wang <kevinj.wang@mail.utoronto.ca>
2021-12-04 21:25:09 -0500

    fixed trivial syntax error

--- src/main/java/Graphics/ExplicitGrapher.java
+++ src/main/java/Graphics/ExplicitGrapher.java
@@ -6,7 +6,7 @@ import Backend.Expressions.RealValuedExpression;
 
 public class ExplicitGrapher {
 
-    public int[] graph(int[] pixels, int w, int h, Evaluatable func, float[] graphData){
+    public void graph(int[] pixels, int w, int h, Evaluatable func, float[] graphData){
         float scale = graphData[0];
         float xpos = graphData[1];
         float ypos = graphData[2];
@@ -16,12 +16,10 @@ public class ExplicitGrapher {
         //For each x coordinate, draw the f(x) coordinates that are on the grid approximating it to scale.
         for (int x = 0; x < w; x++) {
             float cx = (x / (float)w) * scale + xpos;
-            System.out.println(w * (int) (Math.floor(func.evaluate(cx) * h / scale)) + x);
-            if((w * (int) (Math.floor(func.evaluate(cx)) * h / scale) + x) > 0) {
+            if((w * (h - (int) (Math.floor(func.evaluate(cx) * h / scale))) + x) > 0 && (w * (h - (int) (Math.floor(func.evaluate(cx) * h / scale))) + x) < pixels.length) {
                 pixels[w * (h - (int) (Math.floor(func.evaluate(cx) * h / scale))) + x] = (int) Long.parseLong("FF000000", 16);
             }
         }
-        return pixels;
     }
 
     public static void graphExplicit(int[] pixels, int w, int h, Axes ax,
--- src/main/java/Graphics/ImplicitGrapher.java
+++ src/main/java/Graphics/ImplicitGrapher.java
@@ -4,7 +4,7 @@ import static Graphics.RGBA.fmtHex255;
 
 /**
  * ImplicitGrapher is responsible for the core 2D graphing functionality.
- *   It writes data to an array representing pixels of an image.
+ * It writes data to an array representing pixels of an image.
  */
 
 public class ImplicitGrapher {
@@ -13,14 +13,15 @@ public class ImplicitGrapher {
 
     /**
      * Writes the graph of a function onto the image represented by pixels.
-     * @param pixels array representing an ARGB image of dimensions (w,h)
-     * @param w width of the image represented by pixels
-     * @param h height of the image represented by pixels
-     * @param func the function to be graphed
+     *
+     * @param pixels    array representing an ARGB image of dimensions (w,h)
+     * @param w         width of the image represented by pixels
+     * @param h         height of the image represented by pixels
+     * @param func      the function to be graphed
      * @param graphData array of {scale, x position, y position}
-     * @param gtype the type of graph to generate
+     * @param gtype     the type of graph to generate
      */
-    public void graph(int[] pixels, int w, int h, Evaluatable func, float[] graphData, GraphType gtype){
+    public void graph(int[] pixels, int w, int h, Evaluatable func, float[] graphData, GraphType gtype) {
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
                 writePixel(pixels, w, h, func, graphData, gtype, x, y);
@@ -29,38 +30,39 @@ public class ImplicitGrapher {
     }
 
     private void writePixel(int[] pixels, int w, int h, Evaluatable func,
-                        float[] graphData, GraphType gtype,
-                        int x, int y) {
+                            float[] graphData, GraphType gtype,
+                            int x, int y) {
 
-      // This can be moved outside the double-for loop
-      // but "premature optimization is the root of all evil"
-      // currently this avoids long parameter list and data clump
-      float scale = graphData[0];
-      float xpos = graphData[1];
-      float ypos = graphData[2];
-      float pixelSize = scale / (float)w;
-
-      // Convert pixel coordinate to graphing-space coordinate
-      float cx = (x / (float)w - 0.5f) * scale + xpos;
-      float cy = -(y / (float)h - 0.5f) * scale + ypos;
-
-      if (gtype == GraphType.REGION) {
-          if (func.evaluate(cx, cy) < 0){
-              pixels[y * w + x] = BLACK;
-          }
-      }
-      else if (gtype == GraphType.GRAYSCALE) {
-          float result = func.evaluate(cx, cy);
-          String outR = fmtHex255((int) (255 * Math.sqrt(result)));
-          pixels[y * w + x] = (int) Long.parseLong("FF" + outR + outR + outR, 16);
-      }
-      else if (gtype == GraphType.BOUNDARY) {
-          if ((func.evaluate(cx, cy) > 0) ^ (func.evaluate(cx + pixelSize, cy) > 0)) {
-              pixels[y * w + x] = BLACK;
-          }
-          if ((func.evaluate(cx, cy) > 0) ^ (func.evaluate(cx, cy + pixelSize) > 0)) {
-              pixels[y * w + x] = BLACK;
-          }
-      }
+        // This can be moved outside the double-for loop
+        // but "premature optimization is the root of all evil"
+        // currently this avoids long parameter list and data clump
+        float scale = graphData[0];
+        float xpos = graphData[1];
+        float ypos = graphData[2];
+        float pixelSize = scale / (float) w;
+        // Convert pixel coordinate to graphing-space coordinate
+        float cx = (x / (float) w - 0.5f) * scale + xpos;
+        float cy = -(y / (float) h - 0.5f) * scale + ypos;
+        if (gtype == GraphType.REGION) {
+            if (func.evaluate(cx, cy) < 0) {
+                pixels[y * w + x] = BLACK;
+            }
+        }
+        else if (gtype == GraphType.GRAYSCALE) {
+            float result = func.evaluate(cx, cy);
+            String outR = fmtHex255((int) (255 * Math.sqrt(result)));
+            pixels[y * w + x] = (int) Long.parseLong("FF" + outR + outR + outR, 16);
+        }
+        else if (gtype == GraphType.BOUNDARY) {
+//            if ((func.evaluate(cx, cy) > 0) ^ (func.evaluate(cx + pixelSize, cy) > 0)) {
+//                pixels[y * w + x] = BLACK;
+//            }
+//            if ((func.evaluate(cx, cy) > 0) ^ (func.evaluate(cx, cy + pixelSize) > 0)) {
+//                pixels[y * w + x] = BLACK;
+//            }
+            if (Math.abs(func.evaluate(cx, cy)) < pixelSize) {
+                pixels[y * w + x] = BLACK;
+            }
+        }
     }
 }
