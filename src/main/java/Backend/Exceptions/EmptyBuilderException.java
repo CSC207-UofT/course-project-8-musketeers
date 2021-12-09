3e9a0be99ce01b0b8af78ce25094d53e1ab75145
Andric0901 <binseong.kim@mail.utoronto.ca>
2021-12-09 00:57:01 -0500

    Remove Empty Builder Exception

--- src/main/java/Backend/Exceptions/EmptyBuilderException.java
+++
@@ -1,7 +0,0 @@
-package Backend.Exceptions;
-
-public class EmptyBuilderException extends InvalidTermException {
-    public EmptyBuilderException(String message) {
-        super(message);
-    }
-}
